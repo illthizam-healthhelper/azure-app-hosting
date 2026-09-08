e7c859f00d0588348a663f0a99730e0a43cec3b5
Nassar Illthizam <illthizam.healthhelper@gmail.com>
2026-09-08 15:24:42 +0530

    everything

--- modules/foundation/outputs.tf
+++ modules/foundation/outputs.tf
@@ -11,12 +11,9 @@ output "management_group_name" {
 output "management_group_display_name" {
   value = azurerm_management_group.this.display_name
 }
+output "subscription_id" {
+  value = data.azurerm_subscription.current.subscription_id
+}
 output "subscription_name" {
-  value = azurerm_subscription.this.subscription_name
-}
-output "billing_scope_id" {
-  value = azurerm_subscription.this.billing_scope_id
-}
-output "workload" {
-  value = azurerm_subscription.this.workload
+  value = data.azurerm_subscription.current.display_name
 }
